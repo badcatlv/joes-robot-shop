e87988a221c6486e450ec0e6e127900db8836701
Colleen Blake <colleenemilyblake@gmail.com>
2024-07-25 15:53:47 -0500

    Add ILineItem

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,17 +1,47 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { ILineItem } from './cart/line-item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
-  cart: IProduct[] = [];  
+  private cart: ILineItem[] = [];
 
   constructor() { }
 
-  add(product: IProduct) {
-    this.cart.push(product);
-    console.log(`Added ${product.name} to cart`);
+  getTotalPrice(): number {
+    return (
+      Math.round(
+        this.cart.reduce<number>((prev, cur) => {
+          return (
+            prev + cur.price * (cur.product.price * (1 - cur.product.discount))
+          );
+        }, 0) * 100
+          ) / 100
+    );
   }
+
+  findLineItem(product : IProduct) {
+    return this.cart.find((item) => item.product.id === product.id);
+  }
+
+  add(product: IProduct) {
+    let lineItem = this.findLineItem(product);
+    if (lineItem !== undefined) {
+      lineItem.quantity++;
+    } else {
+      lineItem = { product: product, quantity: 1 };
+      this.cart.push({ product, quantity: 1 });
+    }
+  }
+
+  // cart: IProduct[] = [];  
+
+  // constructor() { }
+
+  // add(product: IProduct) {
+  //   this.cart.push(product);
+  //   console.log(`Added ${product.name} to cart`);
+  // }
 }
---
+++ src/app/catalog/line-item.model.ts
@@ -0,0 +1,8 @@
+import { IProduct } from './product.model';
+
+export interface ILineItem {
+    product: IProduct;
+    quantity: number;
+  }
+  
+  
