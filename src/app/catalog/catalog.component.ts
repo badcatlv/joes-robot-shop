b37ff1dfb924bd110362114f741e50f5785cea01
Colleen Blake <colleenemilyblake@gmail.com>
2024-07-26 15:09:25 -0500

    Using routerLink, attribute binding

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -14,7 +15,9 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService // Inject the ProductService, which is a service that fetches the product data
+    private productSvc: ProductService, // Inject the ProductService, which is a service that fetches the product data
+    private router: Router, // Inject the Router service, which is used to navigate to different routes
+    private route: ActivatedRoute // Inject the ActivatedRoute service, which is used to access the route parameters
   ) { }
 
   ngOnInit() {
@@ -22,10 +25,15 @@ export class CatalogComponent {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
+    this.route.params.subscribe((params) => {
+      this.filter = params['filter'];
+    }); // Subscribe to the route params, and update the filter property when the filter parameter changes
+    //this.filter = this.route.snapshot.params['filter']; // Get the filter parameter from the route, note that this only works when the component is initialized
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']); // Navigate to the cart route when a product is added to the cart
   }
 
   getFilteredProducts() {
