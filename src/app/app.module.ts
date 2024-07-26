ade6ad6ca5511e7a5b14cb7037f3d5c0e363c78c
Colleen Blake <colleenemilyblake@gmail.com>
2024-07-26 12:19:30 -0500

    Updated code copied from original repo, fix break that occured when trying to manually update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
@@ -12,17 +12,14 @@ import { CartComponent } from './cart/cart.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent,
     HomeComponent,
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
-    CartComponent,
+    CartComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
