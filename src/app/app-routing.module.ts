dd5f5c743395802a0688cf4f12ed6b505d3b236f
Colleen Blake <colleenemilyblake@gmail.com>
2024-07-29 09:39:22 -0500

    Walk through with template forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home - Joe's Robot shop" }, //path: 'home' is the URL path that will trigger the HomeComponent to be displayed
   { path: 'catalog', component: CatalogComponent, title: "Catalog - Joe's Robot shop" },
   { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot shop" }, // Add a route for the CartComponent, etc.
+  { path: 'sign-in', component: SignInComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect the root URL to the home route
   //order of routes is important, the first match is the one that is used, so the wildcard route should be the last one
 
