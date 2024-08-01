f4c8184742328ed90fb2dd2a012ebc4e9f932dbf
Colleen Blake <colleenemilyblake@gmail.com>
2024-08-01 10:01:06 -0500

    Add validation to template form

--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-sign-in',
@@ -6,7 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
+  credentials: IUserCredentials = {
+    email: '',
+    password: '',
+  };
+  
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
+
+  signIn() {
+    this.userService.signIn(this.credentials).subscribe({
+      next: () => {
+        this.router.navigate(['/catalog']);
+            },
+      error: (error) => {
+        console.error('Error signing in', error);
+      },
+    });
+  }
 
 }
